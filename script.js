2406db417f2a7660f8e0d627b23f11d4168417b5
webdev229 <webd229@gmail.com>
2024-03-19 16:09:59 +0700

    hay88net

---
+++ script.js
@@ -0,0 +1,28 @@
+// Show and hide menu
+    function OpenNav(){
+    document.getElementById("Nav").style.width = "100%";
+    }
+    function CloseNav(){
+    document.getElementById("Nav").style.width = "0%";
+    }
+
+    // ========================================================================================
+    // Get the button
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
